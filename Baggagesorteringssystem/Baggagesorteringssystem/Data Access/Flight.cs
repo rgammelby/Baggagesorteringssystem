0da4353bd949b9151f4805830296ad468b456d8d
Tian70777 <158053422+Tian70777@users.noreply.github.com>
2024-05-16 22:19:16 +0200

    Update Flight.cs
    Added properties and fields for
    	- Terminal
    	- Gate
    	- List<FrontDesk>

--- Baggagesorteringssystem/Baggagesorteringssystem/Data Access/Flight.cs
+++ Baggagesorteringssystem/Baggagesorteringssystem/Data Access/Flight.cs
@@ -16,6 +16,29 @@ namespace Baggagesorteringssystem.Data_Access
         private int _aircraftId;
         private int _destinationId;
 
+         // Add properties that will be generated later based on flight plan/ flightManager 
+         // set the values later when known
+         private Terminal _terminal; // Added
+         private Gate _gate; // Added
+         private List<FrontDesk> _frontDesks; // Added
+         public Terminal Terminal
+         {
+             get { return _terminal; }
+             set { _terminal = value; }
+         }
+        
+         public Gate Gate
+         {
+             get { return _gate; }
+             set { _gate = value; }
+         }
+         public List<FrontDesk> FrontDesks // Added
+         {
+             get { return _frontDesks; }
+             set { _frontDesks = value; }
+         }
+
+
         public Flight(int flightId, int flightNumber, (byte, byte) departureTime, (byte, byte) arrivalTime, int airlineId, int aircraftId, int destinationId)
         {
             _flightId = flightId;
